1e1f1e8e58d0a7537094a03875fefd582bc41dd1
PetrPuncman <Petr.Puncman@seznam.cz>
2025-06-13 15:32:00 +0200

    added Person model (needs validation?)

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WsCrud.Controllers
+{
+    [ApiController]
+    [Route("api/persons")]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Hello, world!"); // placeholder
+    }
+}
